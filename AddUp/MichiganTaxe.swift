924647a36de7253339c6597739cdcd7cccffc693
eleves <eleves@mac204-10.local>
2017-10-30 15:18:20 -0400

    version_1

--- AddUp/MichiganTaxe.swift
+++ AddUp/MichiganTaxe.swift
@@ -1,16 +1,15 @@
 //=================================
 import Foundation
 //=================================
-class QuebecTaxes {
+class MichiganTaxe {
     //# MARK: - Properties
     var gst: Float!
     var qst: Float!
     
     //# MARK: - getTaxes
     func getTaxes(initialAmount: Float) -> String {
-        self.gst = initialAmount * 0.05
-        self.qst = initialAmount * 0.09975
-        return String(format: "%.2f", (self.gst + self.qst))
+        self.theTaxe = initialAmount * 0.06
+        return String(format: "%.2f", self.theTaxe)
     }
     
     //# MARK: - getAmountWithTaxes
